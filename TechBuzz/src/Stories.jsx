6eac34c38d054bf07286f1fd653ced747a776582
07-ankur <im4ankur@gmail.com>
2023-12-22 15:55:37 +0530

    Added styling

--- TechBuzz/src/Stories.jsx
+++ TechBuzz/src/Stories.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from "react";
 import { useGlobalContext } from "./context";
+import "./App.css";
 
 const Stories = () => {
   const { hits, isLoading } = useGlobalContext();
@@ -10,16 +11,27 @@ const Stories = () => {
 
   return (
     <>
-      <h2>Tech Buzz Posts</h2>
-      {hits.map((curPost) => {
-        return (
-          <>
-            {" "}
-            <h2>{curPost.title}</h2>
-            <p>{curPost.author}</p>
-          </>
-        );
-      })}
+      <div className="stories-div">
+        <h2>Tech Buzz Posts</h2>
+        {hits.map((curPost) => {
+          const { title, author, objectId, url, num_comments } = curPost;
+          return (
+            <div className="card">
+              <h2>{title}</h2>
+              <p>
+                By <span> {author} </span> | <span> {num_comments} </span>{" "}
+                Comments
+              </p>
+              <div className="card-button">
+                <a href={url} target="blank">
+                  Read More
+                </a>
+                <a href="#">Remove</a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
